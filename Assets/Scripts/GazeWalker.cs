13a71c5aa21453f4d85d9a7ad9cc0a67747953ce
Bentleyj <jgbentley10@gmail.com>
2018-01-18 17:23:31 +0000

    Add walking and stopping script to one character on mouse click

---
+++ Assets/Scripts/GazeWalker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeWalker : MonoBehaviour {
+
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		if (anim == null)
+			anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("1"))
+		{
+			anim.SetBool("Walk", true);
+		} else if(Input.GetKeyDown("2"))
+		{
+			anim.SetBool("Walk", false);
+		}
+	}
+}
